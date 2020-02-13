8ccdd7741791604aa34edda88aca7f9e5d3193f2
Tommy Lai <whlai@avidxchange.com>
2020-02-13 09:57:02 -0700

    Add Delegate Action feature

---
+++ Playground/Methods/DelegateActionExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    public static class DelegateActionExample
+    {
+
+        public static void TestSimpleVersion()
+        {
+            Action<int, int, int> display = (x, y, z) => Console.WriteLine("x: {0}, y: {1}, z: {2}", x, y, z);    // Action<> similar to Func<>, except there is not return output, just void
+            display(100, 200, 300);
+            Console.ReadLine();
+        }
+    }
+}
